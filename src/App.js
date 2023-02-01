b9a635d202a59fbc79fa6bef96b1f5759975aa9e
LiberVance <libervance@LiberVanceui-MacBookPro.local>
2023-02-01 18:24:00 +0900

    [Feat] worldland network smartcontract connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Box, Button, ChakraProvider, Flex, Input } from '@chakra-ui/react'
-import { useState, useEffect } from 'react';
+import { Button, ChakraProvider, Flex, Input } from '@chakra-ui/react'
+import { useState } from 'react';
 import add_abi from './abis/add_abi.json'
 import Web3 from 'web3'
 
@@ -10,17 +10,15 @@ function App() {
   const [account, setAccount] = useState(null)
   const [web3, setWeb3] = useState(null)
 
-  useEffect(() => {
-    (async () => {
-      const [address] = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      })
-
-      setAccount(address)
-      const web3 = new Web3(window.ethereum)
-      setWeb3(web3)
-    })()
-  }, [])
+  const useWeb3 = async () => {
+    const [address] = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })
+    setAccount(address)
+    const web3 = new Web3(window.ethereum)
+    setWeb3(web3)
+    console.log(web3);
+  }
 
   const handleInput1 = (e) => {
     setInput1(e.target.value);
@@ -31,7 +29,7 @@ function App() {
   };
 
   const handleAdd = async () => {
-    const CA = '0x354Ffa4F63848243AcA67480280953206EF66c31'
+    const CA = '0x717F7Da4523EC8BD7Bd79565d9c4f9414114CD60'
     const ABI = add_abi
     const deployed = new web3.eth.Contract(ABI, CA)
     const data = deployed.methods.add(input1, input2).encodeABI()
@@ -72,8 +70,8 @@ function App() {
         <Button backgroundColor="green">+</Button>
         <Input width="150px" border="2px solid black" backgroundColor="white" value={input2} onChange={handleInput2} />
         <Button backgroundColor="black" color="white" onClick={handleAdd}>=</Button>
-        <Button>Result: {result}</Button>
-        {/* <Button>지갑연결</Button> */}
+        <Button backgroundColor="silver">Result: {result}</Button>
+        <Button backgroundColor="orange" onClick={useWeb3}>지갑연결</Button>
       </Flex>
       <Flex width="1000px" justifyContent="center" alignItems="center">Connecting Wallet : {account}</Flex>
     </ChakraProvider>
@@ -82,3 +80,4 @@ function App() {
 
 export default App;
 
+// shell task awkward dinner link nose hope wife soon cute seat curtain
